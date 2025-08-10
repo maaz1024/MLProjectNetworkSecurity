f06234a60d9a9981e9ce797da0a1329f9df53a13
maaz1024 <maaz161200@gmail.com>
2025-08-10 18:04:36 -0400

    Model Trainer Implemented

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 
+
 """
 defining common constant variable for training pipeline
 """
@@ -19,6 +20,7 @@ SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 SAVED_MODEL_DIR =os.path.join("saved_models")
 MODEL_FILE_NAME = "model.pkl"
 
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -28,6 +30,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
 
 """
 Data Validation related constant start with DATA_VALIDATION VAR NAME
@@ -46,7 +49,6 @@ Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
-
 ## kkn imputer to replace nan values
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values": np.nan,
@@ -54,14 +56,12 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "weights": "uniform",
 }
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
-
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 
 """
 Model Trainer ralated constant start with MODE TRAINER VAR NAME
 """
-
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
