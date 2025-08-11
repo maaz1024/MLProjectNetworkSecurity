d41b004409c7b27a996e2bc26db50ca9ed37876f
maaz1024 <maaz161200@gmail.com>
2025-08-10 21:56:01 -0400

    Pipeline completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -23,6 +23,10 @@ from sklearn.ensemble import (
 )
 
 import mlflow
+import dagshub
+import joblib
+dagshub.init(repo_owner='maaz1024', repo_name='MLProjectNetworkSecurity', mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -41,7 +45,10 @@ class ModelTrainer:
             mlflow.log_metric("f1_score",f1_score)
             mlflow.log_metric("precision",precision_score)
             mlflow.log_metric("recall_score",recall_score)
-            mlflow.sklearn.log_model(best_model,"model")
+            #mlflow.sklearn.log_model(best_model,"model")
+            # Save model locally
+            joblib.dump(best_model, "model.pkl")
+            mlflow.log_artifact("model.pkl")
 
         
     def train_model(self,X_train,y_train,x_test,y_test):
